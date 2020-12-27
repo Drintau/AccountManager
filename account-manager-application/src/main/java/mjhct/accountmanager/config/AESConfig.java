17c70951619c5a737d072ebd23b3140d70888557
mjhct <mjhct0358@163.com>
2020-12-27 17:07:07 +0800

    [0.0.1](feat):多环境配置

--- account-manager-application/src/main/java/mjhct/accountmanager/config/AESConfig.java
+++ account-manager-application/src/main/java/mjhct/accountmanager/config/AESConfig.java
@@ -2,14 +2,19 @@ package mjhct.accountmanager.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotEmpty;
 
 /**
  * AES加解密配置类
  */
 @Component
 @ConfigurationProperties(prefix = "aes")
+@Validated
 public class AESConfig {
 
+    @NotEmpty(message = "请配置密钥")
     private String key;
 
     public String getKey() {
