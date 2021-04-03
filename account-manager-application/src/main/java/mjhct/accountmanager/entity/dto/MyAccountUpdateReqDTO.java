ec99257aafc474908f5459c3ea835fec52eb54b8
mjhct <mjhct0358@163.com>
2021-04-03 15:51:55 +0800

    [1.0.1](fix):去除密码校验，因为有的应用就是要求密码只能是数字

--- account-manager-application/src/main/java/mjhct/accountmanager/entity/dto/MyAccountUpdateReqDTO.java
+++ account-manager-application/src/main/java/mjhct/accountmanager/entity/dto/MyAccountUpdateReqDTO.java
@@ -31,8 +31,8 @@ public class MyAccountUpdateReqDTO {
 
     @JsonProperty("password")
     @NotEmpty(message = "密码不能为空")
-    @Pattern(regexp = "^(?![a-z0-9]{6,12})(?![0-9A-Z]{6,12})(?![a-zA-Z]{6,12})[a-zA-Z0-9].{5,11}$",
-            message = "密码长度6-12位，不能以特殊符号开头，且前6位不能仅有：小写字母+数字、大写字母+数字、大小写字母")
+//    @Pattern(regexp = "^(?![a-z0-9]{6,12})(?![0-9A-Z]{6,12})(?![a-zA-Z]{6,12})[a-zA-Z0-9].{5,11}$",
+//            message = "密码长度6-12位，不能以特殊符号开头，且前6位不能仅有：小写字母+数字、大写字母+数字、大小写字母")
     private String myPassword;
 
     @JsonProperty("remark")
