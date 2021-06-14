1a2e128d91b646a67862b72b9b99caa7e25a33e6
mjhct <mjhct0358@163.com>
2021-06-14 17:04:32 +0800

    [1.0.4](feat):获取一个随机密码的接口

---
+++ code/src/main/java/mjhct/accountmanager/controller/PasswordController.java
@@ -0,0 +1,31 @@
+package mjhct.accountmanager.controller;
+
+import mjhct.accountmanager.commons.CommonCode;
+import mjhct.accountmanager.commons.CommonResult;
+import mjhct.accountmanager.service.myaccount.MyPasswordService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/password")
+@CrossOrigin
+public class PasswordController {
+
+    private static final Logger logger = LoggerFactory.getLogger(PasswordController.class);
+
+    @Resource(name = "myPasswordService")
+    private MyPasswordService myPasswordService;
+
+    @GetMapping("/get")
+    public CommonResult<String> getRandomPassword() {
+        String randomPassword = myPasswordService.getRandomPassword(10);
+        return new CommonResult<>(CommonCode.SUCCESS, CommonCode.SUCCESS.message, randomPassword);
+    }
+
+}
