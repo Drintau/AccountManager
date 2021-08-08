c5f20e0179906cdb0c7d6a4e14bba605ca8fb858
mjhct <mjhct0358@163.com>
2021-08-08 23:37:14 +0800

    [1.0.6](release):V1.0.6发布

--- doc/release/generatekey.bat
+++ doc/release/generatekey.bat
@@ -3,6 +3,6 @@
 chcp 65001
 echo 正在生成秘钥...
 
-start java -jar account-manager-1.0.5.jar --securityKey
+start java -jar account-manager-1.0.6.jar --securityKey
 
 rem pause
--- doc/release/startam.bat
+++ doc/release/startam.bat
@@ -3,6 +3,6 @@
 chcp 65001
 echo 正在启动account-manager...
 
-start java -jar account-manager-1.0.5.jar
+start java -jar account-manager-1.0.6.jar
 
 rem pause
