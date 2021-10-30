e77375c0d91170fcefa19c25528b92eaa59266e9
mjhct <mjhct0358@163.com>
2021-10-30 17:10:54 +0800

    [1.0.9](release):V1.0.9发布

--- doc/release/generatekey.bat
+++ doc/release/generatekey.bat
@@ -3,6 +3,6 @@
 chcp 65001
 echo 正在生成秘钥...
 
-start java -jar AccountManager.jar --securityKey
+start java -jar account-manager.jar --securityKey
 
 rem pause
--- doc/release/startam.bat
+++ doc/release/startam.bat
@@ -3,8 +3,8 @@
 chcp 65001
 echo 正在启动account-manager...
 
-start java -jar AccountManager.jar
+start java -jar account-manager.jar
 rem 如果要启动完自动调用系统浏览器访问，就注释上面的命令，放开下面的命令
-rem start java -Djava.awt.headless=false -jar AccountManager.jar
+rem start java -Djava.awt.headless=false -jar account-manager.jar
 
 rem pause
