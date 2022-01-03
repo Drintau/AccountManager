0ed4517f72a76dc516eaab0a77c4cf5da51523ec
mjhct <mjhct0358@163.com>
2022-01-03 15:24:58 +0800

    [1.1.0](feat):移除hutool依赖，加解密重写

--- code/src/main/java/mjhct/accountmanager/util/SecureUtil.java
+++ code/src/main/java/mjhct/accountmanager/util/SecureUtil.java
@@ -1,69 +1,49 @@
 package mjhct.accountmanager.util;
 
-import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 /**
  * 加解密工具类
+ * 秘钥：aes秘钥字节数组->Base64编码
+ * 加密：明文字节数组->aes加密->Base64编码
+ * 解密：密文字符串->Base64解密->aes解密
  */
 public class SecureUtil {
 
-    public static void main(String[] args) throws Exception {
-        // 原文:
-        String message = "Hello, world!";
-        System.out.println("Message: " + message);
-        // 128位密钥 = 16 bytes Key:
-        byte[] key = genAesSecret();
-        System.out.println("秘钥Base64编码：" + Base64.getEncoder().encodeToString(key));
-        // 加密:
-        byte[] data = message.getBytes("UTF-8");
-        byte[] encrypted = aesEncrypt(key, data);
-        System.out.println("Encrypted: " + Base64.getEncoder().encodeToString(encrypted));
-        // 解密:
-        byte[] decrypted = aesDecrypt(key, encrypted);
-        System.out.println(decrypted);
-        System.out.println("Decrypted: " + new String(decrypted, "UTF-8"));
-    }
-
     /**
-     * 生成随机AES秘钥
+     * 生成秘钥
      * @return
-     * @throws Exception
      */
-    public static byte[] genAesSecret() throws Exception {
-        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-        keyGenerator.init(128);
-        SecretKey secretKey = keyGenerator.generateKey();
-        return secretKey.getEncoded();
+    public static String genSecureKey() throws Exception {
+        byte[] key = AesUtil.genAesKey();
+        return Base64.getEncoder().encodeToString(key);
     }
 
     /**
-     * AES加密
+     * 加密
+     * @param key
+     * @param input 明文
+     * @return
+     */
+    public static String encrypt(byte[] key, String input) throws Exception {
+        // 明文转换为字节数组，加密得到密文字节数组
+        byte[] output = AesUtil.aesEncrypt(key, input.getBytes(StandardCharsets.UTF_8));
+        // Base64编码密文
+        return Base64.getEncoder().encodeToString(output);
+    }
+
+    /**
+     * 解密
      * @param key
      * @param input
      * @return
      */
-    public static byte[] aesEncrypt(byte[] key, byte[] input) throws Exception {
-        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-        SecretKey keySpec = new SecretKeySpec(key, "AES");
-        cipher.init(Cipher.ENCRYPT_MODE, keySpec);
-        return cipher.doFinal(input);
-    }
-
-    /**
-     * AES解密
-     * @param key
-     * @param input
-     * @return
-     */
-    public static byte[] aesDecrypt(byte[] key, byte[] input) throws Exception {
-        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-        SecretKey keySpec = new SecretKeySpec(key, "AES");
-        cipher.init(Cipher.DECRYPT_MODE, keySpec);
-        return cipher.doFinal(input);
+    public static String decrypt(byte[] key, String input) throws Exception {
+        // 密文Base64解码得到二进制字节数组,解密得到明文字节数组
+        byte[] output = AesUtil.aesDecrypt(key, Base64.getDecoder().decode(input));
+        // 明文字符串展示
+        return new String(output, StandardCharsets.UTF_8);
     }
 
 }
