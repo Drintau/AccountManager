564cfe4fc4af20063dd9a4f6bd833612701db198
mjhct <mjhct0358@163.com>
2022-01-11 22:21:20 +0800

    [1.1.1](feat):移除jpa，引入spring-data-jdbc，启动成功，但是执行sql报错，自动生成的sql有问题

--- code/src/main/java/mjhct/accountmanager/dao/MyAccountRepository.java
+++ code/src/main/java/mjhct/accountmanager/dao/MyAccountRepository.java
@@ -3,9 +3,13 @@ package mjhct.accountmanager.dao;
 import mjhct.accountmanager.domain.entity.MyAccountPO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface MyAccountRepository extends JpaRepository<MyAccountPO, Integer> {
+import java.util.List;
+
+public interface MyAccountRepository extends PagingAndSortingRepository<MyAccountPO, Integer> {
+
+    List<MyAccountPO> findAll();
 
     // 需要自己拼%
     Page<MyAccountPO> findByAppNameLike(String appName, Pageable pageable);
--- code/src/main/java/mjhct/accountmanager/domain/entity/MyAccountPO.java
+++ code/src/main/java/mjhct/accountmanager/domain/entity/MyAccountPO.java
@@ -1,112 +1,27 @@
 package mjhct.accountmanager.domain.entity;
 
-import javax.persistence.*;
+import lombok.Data;
+import org.springframework.data.relational.core.mapping.Table;
+
 import java.time.OffsetDateTime;
 
-@Entity
-@Table(name = "my_account")
+@Data
+@Table("my_account")
 public class MyAccountPO {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "app_name", length = 20, nullable = false)
     private String appName;
 
-    @Column(name = "app_url", length = 100)
     private String appUrl;
 
-    @Column(name = "my_username", length = 200, nullable = false)
     private String myUsername;
 
-    @Column(name = "my_password", length = 200, nullable = false)
     private String myPassword;
 
-    @Column(name = "remark", length = 200)
     private String remark;
 
-    @Column(name = "create_time")
     private OffsetDateTime createTime;
 
-    @Column(name = "update_time")
     private OffsetDateTime updateTime;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getAppName() {
-        return appName;
-    }
-
-    public void setAppName(String appName) {
-        this.appName = appName;
-    }
-
-    public String getAppUrl() {
-        return appUrl;
-    }
-
-    public void setAppUrl(String appUrl) {
-        this.appUrl = appUrl;
-    }
-
-    public String getMyUsername() {
-        return myUsername;
-    }
-
-    public void setMyUsername(String myUsername) {
-        this.myUsername = myUsername;
-    }
-
-    public String getMyPassword() {
-        return myPassword;
-    }
-
-    public void setMyPassword(String myPassword) {
-        this.myPassword = myPassword;
-    }
-
-    public String getRemark() {
-        return remark;
-    }
-
-    public void setRemark(String remark) {
-        this.remark = remark;
-    }
-
-    public OffsetDateTime getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(OffsetDateTime createTime) {
-        this.createTime = createTime;
-    }
-
-    public OffsetDateTime getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(OffsetDateTime updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    @Override
-    public String toString() {
-        return "MyAccountPO{" +
-                "id=" + id +
-                ", appName='" + appName + '\'' +
-                ", appUrl='" + appUrl + '\'' +
-                ", myUsername='" + myUsername + '\'' +
-                ", myPassword='" + myPassword + '\'' +
-                ", remark='" + remark + '\'' +
-                ", createTime=" + createTime +
-                ", updateTime=" + updateTime +
-                '}';
-    }
 }
