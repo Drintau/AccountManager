a05433c1c56022766d8840382281dfc3c0d9044c
mjhct <mjhct0358@163.com>
2022-01-13 22:13:18 +0800

    [1.1.1](fix):H2对SQL大小写处理跟MySQL不同，需要遵循SQL标准来写sql语句

--- code/src/main/java/mjhct/accountmanager/domain/entity/MyAccountPO.java
+++ code/src/main/java/mjhct/accountmanager/domain/entity/MyAccountPO.java
@@ -1,6 +1,7 @@
 package mjhct.accountmanager.domain.entity;
 
 import lombok.Data;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.OffsetDateTime;
@@ -9,19 +10,27 @@ import java.time.OffsetDateTime;
 @Table("my_account")
 public class MyAccountPO {
 
+    @Column("id")
     private Integer id;
 
+    @Column("app_name")
     private String appName;
 
+    @Column("app_url")
     private String appUrl;
 
+    @Column("my_username")
     private String myUsername;
 
+    @Column("my_password")
     private String myPassword;
 
+    @Column("remark")
     private String remark;
 
+    @Column("create_time")
     private OffsetDateTime createTime;
 
+    @Column("update_time")
     private OffsetDateTime updateTime;
 }
