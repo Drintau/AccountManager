633da67329958618cea547c57e5db6d944b50f08
mjhct <mjhct0358@163.com>
2022-03-26 11:44:50 +0800

    [1.1.3](feat):密码字符优化

--- code/src/main/java/mjhct/accountmanager/service/myaccount/impl/MyPasswordServiceImpl.java
+++ code/src/main/java/mjhct/accountmanager/service/myaccount/impl/MyPasswordServiceImpl.java
@@ -16,12 +16,14 @@ public class MyPasswordServiceImpl implements MyPasswordService {
     private static final List<String> passwordCharList;
 
     static {
-        // 去除一些较难分辨的字符，如：O、o、0、l、|、\、/ ……
+        // 去除一些较难分辨的字符，如：
+        // O、o、0、
+        // l、|（竖线）、\、/、 I（浏览器显示和小写l一样）
         String[] passwordCharArray = new String[]{
-                "A","B","C","D","E","F","G","H","I","J","K","L","M","N","P","Q","R","S","T","U","V","W","X","Y","Z",
+                "A","B","C","D","E","F","G","H","J","K","L","M","N","P","Q","R","S","T","U","V","W","X","Y","Z",
                 "a","b","c","d","e","f","g","h","i","j","k","m","n","p","q","r","s","t","u","v","w","x","y","z",
-                "2","3","4","5","6","7","8","9",
-                "@","#","$","%","&","+","-","*","=","_",".",":","?","(",")","[","]","<",">"
+                "1","2","3","4","5","6","7","8","9",
+                "@","#","$","%","&","+","-","*","=","_",".",":","?","(",")","[","]","<",">","{","}"
         };
         passwordCharList = List.of(passwordCharArray);
     }
