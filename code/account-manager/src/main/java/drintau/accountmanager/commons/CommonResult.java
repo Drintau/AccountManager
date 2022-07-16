a14135559b49466d7b3f9bbadaaa7409a984dad4
drintau <drintau@163.com>
2022-07-16 14:59:04 +0800

    [1.2.0](style):删除无用类，整理代码

--- code/account-manager/src/main/java/drintau/accountmanager/commons/CommonResult.java
+++ code/account-manager/src/main/java/drintau/accountmanager/commons/CommonResult.java
@@ -1,15 +1,15 @@
 package drintau.accountmanager.commons;
 
+import lombok.Data;
+
 import java.io.Serializable;
 
+@Data
 public class CommonResult<T> implements Serializable {
 
     private String code;
     private String message;
     private T data;
-
-    public CommonResult() {
-    }
 
     public CommonResult(CommonCode commonCode, String message) {
         this.code = commonCode.code;
@@ -35,27 +35,4 @@ public class CommonResult<T> implements Serializable {
         this.data = data;
     }
 
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public T getData() {
-        return data;
-    }
-
-    public void setData(T data) {
-        this.data = data;
-    }
 }
