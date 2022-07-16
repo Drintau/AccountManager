fa9aed7f1543735b166a4ff0444b3deb5d4399fc
drintau <drintau@163.com>
2022-07-16 15:31:08 +0800

    [1.2.0](feat):测试备份数据库文件成功

--- code/account-manager/src/main/java/drintau/accountmanager/service/myaccount/impl/BackupServiceImpl.java
+++ code/account-manager/src/main/java/drintau/accountmanager/service/myaccount/impl/BackupServiceImpl.java
@@ -1,5 +1,6 @@
 package drintau.accountmanager.service.myaccount.impl;
 
+import com.zaxxer.hikari.HikariDataSource;
 import drintau.accountmanager.config.AccountManagerConfig;
 import drintau.accountmanager.service.myaccount.BackupService;
 import drintau.accountmanager.util.DateTimeUtil;
@@ -7,6 +8,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.file.Files;
 
 @Slf4j
 @Service
@@ -15,11 +20,26 @@ public class BackupServiceImpl implements BackupService {
     @Resource
     private AccountManagerConfig accountManagerConfig;
 
+    @Resource
+    private HikariDataSource dataSource;
+
     @Override
     public void startBackup() {
         log.debug("开始备份:{}", DateTimeUtil.nowChinaOffsetDateTime());
+        log.debug("db路径:{}", accountManagerConfig.getFilePath());
         log.debug("备份路径:{}", accountManagerConfig.getBackupPath());
-        // 读取数据库文件
-        // 写入到备份路径
+        try {
+            dataSource.close();
+            // 读取数据库文件
+            File dbFile = new File(accountManagerConfig.getFilePath() + ".mv.db");
+            // 写入到备份路径
+            File backupFile = new File(accountManagerConfig.getBackupPath() + ".mv.db");
+            backupFile.createNewFile();
+            Files.copy(dbFile.toPath(), new BufferedOutputStream(new FileOutputStream(backupFile)));
+        } catch (Exception e) {
+            log.error("备份失败", e);
+            throw new RuntimeException(e);
+        }
+
     }
 }
