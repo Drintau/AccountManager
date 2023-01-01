774eadfe9c359bd1a2c2ad968ece8547616ae3b4
Drintau <1963704754@qq.com>
2023-01-01 20:19:01 +0800

    [1.2.0](feat):退出程序前，备份数据功能

--- code/account-manager/src/main/java/drintau/accountmanager/service/myaccount/impl/BackupServiceImpl.java
+++ code/account-manager/src/main/java/drintau/accountmanager/service/myaccount/impl/BackupServiceImpl.java
@@ -1,6 +1,8 @@
 package drintau.accountmanager.service.myaccount.impl;
 
 import drintau.accountmanager.config.AccountManagerConfig;
+import drintau.accountmanager.dao.MyAccountRepository;
+import drintau.accountmanager.domain.entity.MyAccountPO;
 import drintau.accountmanager.service.myaccount.BackupService;
 import drintau.accountmanager.util.DateTimeUtil;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +12,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 
 import javax.annotation.PreDestroy;
 import java.io.BufferedReader;
@@ -31,6 +34,9 @@ public class BackupServiceImpl implements BackupService {
     @Autowired
     private AccountManagerConfig accountManagerConfig;
 
+    @Autowired
+    private MyAccountRepository myAccountRepository;
+
     @PreDestroy
     @Override
     public void backup() {
@@ -41,6 +47,54 @@ public class BackupServiceImpl implements BackupService {
         if (CollectionUtils.isEmpty(backupPaths)) {
             return;
         }
+
+        // 要备份的数据
+        List<MyAccountPO> allData = myAccountRepository.list();
+        if (CollectionUtils.isEmpty(allData)) {
+            return;
+        }
+        StringBuilder insertSql1 = new StringBuilder("insert into my_account(app_name, app_url, my_username, my_password, remark, create_time, update_time) values ");
+        String nullSql = "null, ";
+        for (MyAccountPO data : allData) {
+            insertSql1.append("(");
+
+            if (StringUtils.hasText(data.getAppName())) {
+                insertSql1.append("'").append(data.getAppName()).append("', ");
+            } else {
+                insertSql1.append(nullSql);
+            }
+
+            if (StringUtils.hasText(data.getAppUrl())) {
+                insertSql1.append("'").append(data.getAppUrl()).append("', ");
+            } else {
+                insertSql1.append(nullSql);
+            }
+
+            if (StringUtils.hasText(data.getMyUsername())) {
+                insertSql1.append("'").append(data.getMyUsername()).append("', ");
+            } else {
+                insertSql1.append(nullSql);
+            }
+
+            if (StringUtils.hasText(data.getMyPassword())) {
+                insertSql1.append("'").append(data.getMyPassword()).append("', ");
+            } else {
+                insertSql1.append(nullSql);
+            }
+
+            if (StringUtils.hasText(data.getRemark())) {
+                insertSql1.append("'").append(data.getRemark()).append("', ");
+            } else {
+                insertSql1.append(nullSql);
+            }
+
+            insertSql1.append("'").append(data.getCreateTime()).append("', ");
+            insertSql1.append("'").append(data.getUpdateTime()).append("'),");
+        }
+        String insertSql = insertSql1.deleteCharAt(insertSql1.length() - 1).toString();
+
+        // 清空数据sql
+        String truncateSql = "TRUNCATE TABLE my_account RESTART IDENTITY;";
 
         try {
             // 读取数据库脚本文件
@@ -64,7 +118,13 @@ public class BackupServiceImpl implements BackupService {
                 PreparedStatement preparedStatement = connection.prepareStatement(dbInit.toString());
                 preparedStatement.execute();
 
+                // 清空表数据
+                preparedStatement = connection.prepareStatement(truncateSql);
+                preparedStatement.execute();
+
                 // 写入数据
+                preparedStatement = connection.prepareStatement(insertSql);
+                preparedStatement.execute();
 
                 connection.close();
             }
