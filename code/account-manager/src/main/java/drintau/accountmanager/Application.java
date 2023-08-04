2d3fe52f7c884f3909ac5b9c960fcbd4e5f39e74
Drintau <1963704754@qq.com>
2023-08-04 23:43:08 +0800

    [1.3.0](feat):引入javafx

--- code/account-manager/src/main/java/drintau/accountmanager/Application.java
+++ code/account-manager/src/main/java/drintau/accountmanager/Application.java
@@ -1,5 +1,6 @@
 package drintau.accountmanager;
 
+import drintau.accountmanager.gui.MainUI;
 import drintau.accountmanager.plugin.*;
 import drintau.accountmanager.webserver.WebServerConfiguration;
 import org.springframework.boot.SpringApplication;
@@ -11,18 +12,19 @@ public class Application {
 
     public static void main(String[] args) {
         // 如果操作系统有图形化桌面，使用图形化；否则是命令行程序
+        javafx.application.Application.launch(MainUI.class, args);
 
 //        List<PluginInterface> startPlugins = new ArrayList<>();
 //        startPlugins.add(new LaunchArgsPlugin(args));
 //        for (PluginInterface startPlugin : startPlugins) {
 //            startPlugin.execute();
 //        }
-        new LaunchArgsPlugin(args).execute();
+//        new LaunchArgsPlugin(args).execute();
 
         /*
          * 通过JVM传参来禁用headless模式 -Djava.awt.headless=false
          */
-        SpringApplication.run(WebServerConfiguration.class, args);
+//        SpringApplication.run(WebServerConfiguration.class, args);
 
 //        List<PluginInterface> endPlugins = new ArrayList<>();
 //        endPlugins.add(new BackupPlugin());
---
+++ code/account-manager/src/main/java/drintau/accountmanager/gui/MainUI.java
@@ -0,0 +1,17 @@
+package drintau.accountmanager.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * 主页面
+ */
+public class MainUI extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("账号管理器");
+        stage.show();
+    }
+
+}
