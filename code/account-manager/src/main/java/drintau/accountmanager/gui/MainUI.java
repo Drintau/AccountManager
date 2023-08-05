876cfc28f282aa61a20313ce3afff55c23d4a294
Drintau <1963704754@qq.com>
2023-08-05 10:50:55 +0800

    [1.3.0](feat):启动、停止按钮

--- code/account-manager/src/main/java/drintau/accountmanager/gui/MainUI.java
+++ code/account-manager/src/main/java/drintau/accountmanager/gui/MainUI.java
@@ -1,6 +1,9 @@
 package drintau.accountmanager.gui;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -10,7 +13,23 @@ public class MainUI extends Application {
 
     @Override
     public void start(Stage stage) {
+
+        // 控件
+        Button stop = new Button("停止");
+        Button start = new Button("启动");
+
+        // 布局
+        VBox vBox = new VBox();
+        vBox.getChildren().addAll(start, stop);
+
+        // 场景
+        Scene scene = new Scene(vBox);
+
+        // 舞台
+        stage.setScene(scene);
         stage.setTitle("账号管理器");
+        stage.setWidth(600);
+        stage.setHeight(400);
         stage.show();
     }
 
