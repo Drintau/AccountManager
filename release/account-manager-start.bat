5155ff0aceddd61b96f30124dc8e667158aabc6e
Drintau <1963704754@qq.com>
2023-12-17 12:43:38 +0800

    [1.3.1](docs):说明文件更新

--- release/account-manager-start.bat
+++ release/account-manager-start.bat
@@ -3,8 +3,8 @@
 chcp 65001
 echo 正在启动account-manager...
 
-start java -jar account-manager.jar
-rem 如果要使用GUI，就注释上面的命令，放开下面的命令
-rem start java -Djava.awt.headless=false -jar account-manager.jar
+start java -Djava.awt.headless=false -jar account-manager.jar
+rem 如果不使用GUI，就注释上面的命令，放开下面的命令
+rem start java -jar account-manager.jar
 
 rem pause
