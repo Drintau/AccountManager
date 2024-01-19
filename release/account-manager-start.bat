6fccac51e8f39041ef2b61cdc045a210f66c30ad
Drintau <1963704754@qq.com>
2024-01-19 16:51:59 +0800

    [1.3.2](docs):文档修复

--- release/account-manager-start.bat
+++ release/account-manager-start.bat
@@ -4,10 +4,10 @@ chcp 65001
 echo 正在启动account-manager...
 
 rem 有命令行窗口，有图形化界面的启动命令
-rem start java --useGUI -jar account-manager.jar
+rem start java -jar account-manager.jar --useGUI
 rem 有命令行窗口，无图形化界面的启动命令
 rem start java -jar account-manager.jar
 rem 无命令行窗口，有GUI的启动命令
-start javaw --useGUI -jar account-manager.jar
+start javaw -jar account-manager.jar --useGUI
 
 rem pause
