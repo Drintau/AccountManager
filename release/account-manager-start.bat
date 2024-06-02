9049fc5c794d9c420d9a333bd18ce474ee75f6c5
Drintau <1963704754@qq.com>
2024-06-02 11:28:31 +0800

    [2.0.0](docs):文档更新

--- release/account-manager-start.bat
+++ release/account-manager-start.bat
@@ -7,7 +7,7 @@ rem 有命令行窗口，有图形化界面的启动命令
 rem start java -jar account-manager.jar --useGUI
 rem 有命令行窗口，无图形化界面的启动命令
 rem start java -jar account-manager.jar
-rem 无命令行窗口，有GUI的启动命令
+rem 无命令行窗口，有图形化界面的启动命令
 start javaw -jar account-manager.jar --useGUI
 
 rem pause
