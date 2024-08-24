08d09fe994d133c65a0eb8be76d1847d236dfd95
Drintau <1963704754@qq.com>
2024-08-24 14:41:11 +0800

    [2.0.0](feat):axios示例代码

--- code/account-manager/src/main/resources/static/index.js
+++ code/account-manager/src/main/resources/static/index.js
@@ -20,6 +20,23 @@ const App = {
             console.log("Hello2");
         },
 
+        // 获取随机密码
+        getRandomPassword() {
+            axios.get('/accountmanager/password/get')
+                .then(function (response) {
+                    console.log(response.data);
+                    console.log(response.data.code);
+                    console.log(response.data.message);
+                    console.log(response.data.data);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+                .finally(function () {
+                    console.log("发出请求");
+                });
+        },
+
     },
 
 }
