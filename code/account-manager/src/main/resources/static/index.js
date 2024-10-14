10137dca9bcef1123989821768bb70e322650912
Drintau <1963704754@qq.com>
2024-10-14 23:09:37 +0800

    [2.0.0](feat):列表可以展示数据了，但是分页没有效果

--- code/account-manager/src/main/resources/static/index.js
+++ code/account-manager/src/main/resources/static/index.js
@@ -1,66 +1,30 @@
 const { createApp } = Vue
 const { createVuetify } = Vuetify
 
-// 同步请求后端接口获取数据的函数，命名方式：api + xxxx
-async function apiQueryList(pageNumber, pageSize, decryptFlag) {
-  try {
-    let response = await axios.post('/accountmanager/account/query', 
-                                {
-                                  page_number: pageNumber,
-                                  page_size: pageSize,
-                                  decrypt: decryptFlag,
-                                  fuzzy_name: null
-                                });
-    let resJson = response.data;
-    console.log("111");
-    console.log(resJson);
-    return resJson;
-  } catch (error) {
-    console.error(error);
-  }
-}
-
 const vuetify = createVuetify()
-
-const FakeAPI = {
-    async fetch ({ page, itemsPerPage, sortBy }) {
-      return new Promise(resolve => {
-        setTimeout(() => {
-          axios.post('/accountmanager/account/query', {
-            page_number: page,
-            page_size: 10,
-            decrypt: true,
-            fuzzy_name: null
-        })
-        .then(function (response) {
-            let resJson = response.data;
-            console.log(resJson);
-            resolve({ items: resJson.data.list, total: resJson.data.total_records })
-        })
-        .catch(function (error) {
-        })
-        .finally(function () {
-        });
-
-        }, 500)
-      })
-    },
-  }
 
 const App = {
 
     data() {
         return {
 
+            // 是否解密
+            decryptFlag: true,
+
+            // 随机密码
+            randomPassword: null,
+
             // 搜索框
             
 
             // 分页
             pageNumber: 1,
-            pageSize: 10,
-            totalRecords: 0,
+            pageSize: 5,
+            pageSizeOptions: [{value: 5, title: '5'},{value: 10, title: '10'}],
+            pageSizeOptionsText: "每页记录数",
+            totalItems: 0,
             
-            // 列表表头、内容
+            // 数据列表
             headers: [
               { key: 'name', title: '名称', sortable: false},
               { key: 'url', title: '网址', sortable: false},
@@ -70,11 +34,10 @@ const App = {
               { key: 'create_time', title: '创建时间', sortable: false},
               { key: 'update_time', title: '更新时间', sortable: false},
             ],
-            serverDatas: [],
-            
+            serverItems: [],
             loading: false,
-            randomPassword: null,
-            decryptFlag: true,
+            
+            
         }
     },
 
@@ -97,42 +60,24 @@ const App = {
         // 根据id查询
 
         // 列表查询
-        loadItems ({ page, pageSize, sortBy }) {
-            this.loading = true;
-            FakeAPI.fetch({ page, pageSize, sortBy }).then(({ items, total }) => {
-                console.log(items);
-              this.serverDatas = items;
-              this.totalRecords = total;
-              this.loading = false;
-            })
-          },
-
-        async test() {
-          let resJson = await apiQueryList(1, 10, true);
-          console.log("222");
-          console.log(resJson);
+        async loadItems() {
+          this.loading = true;
+          try {
+            let response = await axios.post('/accountmanager/account/query',
+                                        {
+                                          page_number: this.pageNumber,
+                                          page_size: this.pageSize,
+                                          decrypt: this.decryptFlag,
+                                          fuzzy_name: null
+                                        });
+            let resJson = response.data;
+            this.serverItems = resJson.data.list;
+            this.totalItems = resJson.data.total_records;
+            this.loading = false;
+          } catch (error) {
+            console.error(error);
+          }
         }
-        // apiQueryList() {
-        //     this.loading = true;
-        //     axios.post('/accountmanager/account/query', {
-        //         page_number: this.pageNumber,
-        //         page_size: this.pageSize,
-        //         decrypt: this.decryptFlag,
-        //         fuzzy_name: null
-        //     })
-        //     .then(function (response) {
-        //         let resJson = response.data;
-        //         console.log(resJson);
-        //         this.loading = false;
-        //         this.serverDatas = [];
-        //         this.totalRecords = resJson.data.total_records;
-                
-        //     })
-        //     .catch(function (error) {
-        //     })
-        //     .finally(function () {
-        //     });
-        // },
 
         // 新增
 
