135d3a7b72061fbfb9bdf44bba6bdefbce605c1e
Drintau <1963704754@qq.com>
2024-12-01 14:04:38 +0800

    [2.0.1](docs):文档更新

--- release/account-manager-start.bat
+++ release/account-manager-start.bat
@@ -5,9 +5,14 @@ echo 正在启动account-manager...
 
 rem 有命令行窗口，有图形化界面的启动命令
 rem start java -jar account-manager.jar --useGUI
+
 rem 有命令行窗口，无图形化界面的启动命令
 rem start java -jar account-manager.jar
+
 rem 无命令行窗口，有图形化界面的启动命令
-start javaw -jar account-manager.jar --useGUI
+rem start javaw -jar account-manager.jar --useGUI
+
+rem 无命令行窗口，有图形化界面的启动命令，限制一下使用内存量
+start javaw -Xms128m -Xmx128m -jar account-manager.jar --useGUI
 
 rem pause
