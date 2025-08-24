e74b2d5ed9a1f24b545681df1967358796ac1936
Drintau <1963704754@qq.com>
2025-08-24 11:23:03 +0800

    [2.0.2](feat):简单的切换展示对应内容

--- code/account-manager/src/main/resources/static/index.js
+++ code/account-manager/src/main/resources/static/index.js
@@ -5,6 +5,29 @@ const vuetify = createVuetify()
 
 const App = {
 
+    data() {
+        return {
+            show1: false,
+            show2: false,
+        }
+    },
+
+    methods: {
+
+        changeShow(num) {
+            if (num == 1) {
+                this.show1 = true;
+                this.show2 = false;
+            } else if (num == 2) {
+                this.show1 = false;
+                this.show2 = true;
+            } else {
+                this.show1 = true;
+                this.show2 = true;
+            }
+        }
+
+    }
 
 }
 
