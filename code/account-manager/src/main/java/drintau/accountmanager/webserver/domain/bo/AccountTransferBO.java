f4aabc4ff0686ea7cc6948a3fdd96ccabfc99f19
Drintau <1963704754@qq.com>
2026-02-27 17:07:15 +0800

    [3.0.0](feat):account导出excel样式调整

--- code/account-manager/src/main/java/drintau/accountmanager/webserver/domain/bo/AccountTransferBO.java
+++ code/account-manager/src/main/java/drintau/accountmanager/webserver/domain/bo/AccountTransferBO.java
@@ -2,10 +2,16 @@ package drintau.accountmanager.webserver.domain.bo;
 
 import lombok.Data;
 import org.apache.fesod.sheet.annotation.ExcelProperty;
+import org.apache.fesod.sheet.annotation.write.style.*;
+import org.apache.fesod.sheet.enums.poi.FillPatternTypeEnum;
 
 /**
  * 迁移：导入+导出
  */
+@HeadStyle(fillPatternType = FillPatternTypeEnum.SOLID_FOREGROUND, fillForegroundColor = 31)
+@HeadFontStyle(fontHeightInPoints = 15)
+@ContentFontStyle(fontHeightInPoints = 13)
+@ColumnWidth(25)
 @Data
 public class AccountTransferBO {
 
