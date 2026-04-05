ef4747e9626112c2df30210c64db0ad9ad39c63c
Drintau <1963704754@qq.com>
2026-04-05 18:52:53 +0800

    [3.0.1](feat):关闭窗口前，展示结束语

--- code/account-manager/src/main/java/drintau/accountmanager/desktop/event/CloseEvent.java
+++ code/account-manager/src/main/java/drintau/accountmanager/desktop/event/CloseEvent.java
@@ -5,39 +5,68 @@ import drintau.accountmanager.launcher.LauncherContext;
 import drintau.accountmanager.shared.DaemonScheduler;
 import drintau.accountmanager.shared.ThreadPool;
 import drintau.accountmanager.shared.util.FileUtil;
+import javafx.application.Platform;
 import javafx.event.EventHandler;
+import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.BooleanUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.util.StringUtils;
 
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
 public class CloseEvent implements EventHandler<WindowEvent> {
 
     private static final String dbFileSuffix = ".mv.db";
 
     @Override
     public void handle(WindowEvent windowEvent) {
+        windowEvent.consume();
         DesktopContext desktopContext = DesktopContext.getInstance();
+        desktopContext.getStopButton().setDisable(true);
+        desktopContext.getOpenBrowserButton().setDisable(true);
+        desktopContext.getStartButton().setDisable(true);
 
-        // 关闭服务
-        ConfigurableApplicationContext webServerContext = desktopContext.getWebServerContext();
-        if (webServerContext != null && webServerContext.isRunning()) {
-            SpringApplication.exit(webServerContext);
-            desktopContext.setWebServerContext(null);
-        }
-
-        LauncherContext launcherContext = LauncherContext.getInstance();
-        if (BooleanUtils.isTrue(launcherContext.getEnableBackup())) {
-            if (StringUtils.hasText(launcherContext.getFilePath()) && CollectionUtils.isNotEmpty(launcherContext.getBackupPaths())) {
-                for (String backupPath : launcherContext.getBackupPaths()) {
-                    FileUtil.copyFile(launcherContext.getFilePath() + dbFileSuffix, backupPath + dbFileSuffix);
-                }
+        DaemonScheduler.getInstance().submitOnceDelayTask(() -> {
+            log.info("应用开始退出，请稍候。");
+            // 关闭服务
+            ConfigurableApplicationContext webServerContext = desktopContext.getWebServerContext();
+            if (webServerContext != null && webServerContext.isRunning()) {
+                SpringApplication.exit(webServerContext);
+                desktopContext.setWebServerContext(null);
             }
-        }
 
-        ThreadPool.getInstance().shutdownNow();
-        DaemonScheduler.getInstance().shutdownNow();
+            LauncherContext launcherContext = LauncherContext.getInstance();
+            if (BooleanUtils.isTrue(launcherContext.getEnableBackup())) {
+                log.info("备份功能：开始");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ignored) {}
+                if (StringUtils.hasText(launcherContext.getFilePath()) && CollectionUtils.isNotEmpty(launcherContext.getBackupPaths())) {
+                    for (String backupPath : launcherContext.getBackupPaths()) {
+                        FileUtil.copyFile(launcherContext.getFilePath() + dbFileSuffix, backupPath + dbFileSuffix);
+                    }
+                }
+                log.info("备份功能：完成");
+            }
+
+            ThreadPool.getInstance().shutdownNow();
+//            DaemonScheduler.getInstance().shutdownNow();
+
+            log.info("感谢使用！再见！");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException ignored) {}
+
+            Platform.runLater(() -> {
+                Stage stage = (Stage) windowEvent.getSource();
+                stage.close();
+            });
+
+        }, 1L, TimeUnit.SECONDS);
     }
 }
