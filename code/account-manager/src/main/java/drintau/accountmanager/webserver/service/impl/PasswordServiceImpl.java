b41802bfabb667d453707ab420a37ad09c37a88a
Drintau <1963704754@qq.com>
2026-05-01 19:36:34 +0800

    [3.2.2](feat):生成密码模块优化

--- code/account-manager/src/main/java/drintau/accountmanager/webserver/service/impl/PasswordServiceImpl.java
+++ code/account-manager/src/main/java/drintau/accountmanager/webserver/service/impl/PasswordServiceImpl.java
@@ -1,13 +1,11 @@
 package drintau.accountmanager.webserver.service.impl;
 
+import drintau.accountmanager.shared.util.RandomUtil;
 import drintau.accountmanager.webserver.config.ConfigKeyConstant;
 import drintau.accountmanager.webserver.service.ConfigService;
 import drintau.accountmanager.webserver.service.PasswordService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
 
 @RequiredArgsConstructor
 @Service
@@ -15,20 +13,20 @@ public class PasswordServiceImpl implements PasswordService {
 
     private final ConfigService configService;
 
-    private static final List<String> passwordCharList;
+    /*
+    去除一些较难分辨的字符，如：
+    O、o、0、
+    l、|（竖线）、\、/、 I（浏览器显示和小写l一样）
 
-    static {
-        // 去除一些较难分辨的字符，如：
-        // O、o、0、
-        // l、|（竖线）、\、/、 I（浏览器显示和小写l一样）
-        String[] passwordCharArray = new String[]{
-                "A","B","C","D","E","F","G","H","J","K","L","M","N","P","Q","R","S","T","U","V","W","X","Y","Z",
-                "a","b","c","d","e","f","g","h","i","j","k","m","n","p","q","r","s","t","u","v","w","x","y","z",
-                "1","2","3","4","5","6","7","8","9",
-                "@","#","&","+","*","=",":","?","<",">"
-        };
-        passwordCharList = List.of(passwordCharArray);
-    }
+    然后去掉不好看的，如:
+    -、_、:
+     */
+    private static final char[] PASSWORD_CHARS = new char[]{
+            'A','B','C','D','E','F','G','H','J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z',
+            'a','b','c','d','e','f','g','h','i','j','k','m','n','p','q','r','s','t','u','v','w','x','y','z',
+            '1','2','3','4','5','6','7','8','9',
+            '@','#','&','+','*','=','?'
+    };
 
     @Override
     public String getRandomPassword() {
@@ -38,11 +36,11 @@ public class PasswordServiceImpl implements PasswordService {
 
     @Override
     public String getRandomPassword(int length) {
-        StringBuilder randomPasswordBuilder = new StringBuilder();
-        String randomChar;
+        StringBuilder randomPasswordBuilder = new StringBuilder(length);
+        char randomChar;
         for (int i=0; i < length; i++) {
-            int index = ThreadLocalRandom.current().nextInt(passwordCharList.size());
-            randomChar = passwordCharList.get(index);
+            int index = RandomUtil.randomInt(PASSWORD_CHARS.length);
+            randomChar = PASSWORD_CHARS[index];
             randomPasswordBuilder.append(randomChar);
         }
         return randomPasswordBuilder.toString();
