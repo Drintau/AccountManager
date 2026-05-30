aa797aacc2f853931987488494b78da6a98be11f
Drintau <1963704754@qq.com>
2026-05-30 21:16:14 +0800

    [3.2.4](docs):优化启动参数文档

--- release/account-manager-start.sh
+++ release/account-manager-start.sh
@@ -9,5 +9,5 @@ java -jar account-manager.jar --am.mode=web
 # 无命令行窗口，有图形化界面的启动命令
 # javaw -jar account-manager.jar --am.mode=gui
 
-# 无命令行窗口，有图形化界面的启动命令，限制内存使用量
-# javaw -Xms128m -Xmx128m -jar account-manager.jar --am.mode=gui
+# 无命令行窗口，有图形化界面的启动命令，单机使用优化启动参数
+# javaw -Xms96m -Xmx96m -XX:MaxMetaspaceSize=128m -XX:MetaspaceSize=64m -jar account-manager.jar --am.mode=gui
